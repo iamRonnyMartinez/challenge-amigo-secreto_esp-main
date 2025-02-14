d4b7ef4b8ac1bba26385e15c772f9430218f238f
iamRonnyMartinez <martinezmenesesronny@gmail.com>
2025-02-14 13:49:50 +0000

    Funcion para agregar nombres

--- app.js
+++ app.js
@@ -3,10 +3,33 @@ let amigos = [];
 
 //Funcion para agregar nombres
 function agregarAmigo() {
-    let input = document.getElementById("amigo");
-    let nombre = input.value.trim();
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.ariaValueMax.trim();
 
+    //Validar que el campo no este vacio
     if (nombre === "") {
         alert("Por favor, inserte un nombre.");
         return;
     }
+
+    //Validar que los nombres no esten duplicados
+    if (amigos.includes(nombreAmigo)) {
+        alert(`El nombre ${nombreAmigo}` ya esta en la lista);
+    }
+
+    //Agregar el nombre al array
+    amigos.push(nombreAmigo);
+
+    //Limpiar el campo de entrada
+    inputAmigo.ariaValueMax = "";
+
+    //Actualizar lista
+    actualizarLista();
+
+}
+
+//Funcion para actualizar la lista
+
+
+//Funcion para seleccionar amigo aleatoriamente
+
