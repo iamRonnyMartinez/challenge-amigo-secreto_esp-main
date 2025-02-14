cae0c9b0ef974c4784834777f2471e5dbc1dc10a
iamRonnyMartinez <martinezmenesesronny@gmail.com>
2025-02-14 10:23:07 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+//Array para almacenar los nombres
+let amigos = [];
+
+//Funcion para agregar nombres
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
