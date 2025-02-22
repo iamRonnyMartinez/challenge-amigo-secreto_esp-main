7118054690b18e4c30262f55f91d97cbed777e6f
iamRonnyMartinez <martinezmenesesronny@gmail.com>
2025-02-22 15:34:42 +0000

    Funcion para agregar nombres

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//Array para almacenar los nombres
+let amigos = [];
+
+//Funcion para agregar nombres
+function agregarAmigo() {
+    let inputAmigo = document.getElementById("amigos");
+    let nombreAmigo = inputAmigo.value.trim();
+
+    //Validar que el campo no este vacio
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    //Validar que los nombres no esten duplicados
+    if (amigos.includes(nombreAmigo)) {
+        alert(`El nombre ${nombreAmigo} ya esta en la lista`);
+        return;
+    }
+
+    //Agregar el nombre al array
+    amigos.push(nombreAmigo);
+
+    //Limpiar el campo de entrada
+    inputAmigo.value = "";
+
+
+
+}
